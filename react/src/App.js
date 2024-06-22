05e1247297ef7ebaf4b672511835977f622a4d8d
DOLONG9 <bagmy2@gmail.com>
2024-06-22 22:05:51 +0900

    react test 5

--- react/src/App.js
+++ react/src/App.js
@@ -5,20 +5,26 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 
   
+
 function App() {
-    const [hello, setHello] = useState('')
-      useEffect(() => {
-        axios.get('http://donggu-1-was-service:8080/test')
-        .then(response => setHello(response.data))
-        .catch(error => console.log(error))
-      }, []);
+  const [hello, setHello] = useState('');
 
-      return (
-          <div>
-              from backend : {hello}
-          </div>
-      );
+  useEffect(() => {
+      axios.get('http://172.30.1.98:8080/test')
+          .then(response => setHello(response.data))
+          .catch(error => console.log(error));
+  }, []);
+
+  return (
+      <div className="App">
+          <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />  {/* 로고 이미지 표시 */}
+              <p>
+                  From backend: {hello}
+              </p>
+          </header>
+      </div>
+  );
 }
-
   
 export default App;
